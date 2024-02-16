6341eaa6a1d689c25cede06d2c1567dac59b489d
BaeCheolHan <joker10421@gmail.com>
2024-02-16 10:38:17 +0900

    redis module

---
+++ infra/redis/src/main/kotlin/kr/pe/hws/stock/redis/repository/DividendInfoRepository.kt
@@ -0,0 +1,9 @@
+package kr.pe.hws.stock.redis.repository
+
+import kr.pe.hws.stock.redis.hash.DividendInfo
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DividendInfoRepository : CrudRepository<DividendInfo, String> {
+}
