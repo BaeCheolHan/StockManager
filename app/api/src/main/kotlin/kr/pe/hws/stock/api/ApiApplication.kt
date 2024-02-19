ec28fdcab5357611ce910826fe4e4fa74389d3b9
BaeCheolHan <joker10421@gmail.com>
2024-02-19 16:16:39 +0900

    :wrench: Changing configuration files.

--- app/api/src/main/kotlin/kr/pe/hws/stock/api/ApiApplication.kt
+++ app/api/src/main/kotlin/kr/pe/hws/stock/api/ApiApplication.kt
@@ -2,8 +2,10 @@ package kr.pe.hws.stock.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication(scanBasePackages = ["kr.pe.hws.stock"])
+@EnableFeignClients(basePackages = ["kr.pe.hws.stock"])
 class ApiApplication
 
 fun main(args: Array<String>) {
